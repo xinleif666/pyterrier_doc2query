dc42fd3e02c37136791121e320ec09962069a345
Xinlei Feng <xinlei_feng@ucsb.edu>
2024-06-11 16:15:42 -0700

    Updated evaluate_result.py

--- rerank/evaluate_result.py
+++ rerank/evaluate_result.py
@@ -3,8 +3,7 @@ from statistics import mean
 from collections import defaultdict
 
 run = defaultdict(dict)
-with open('modify0_non.tsv', 'r') as f:
-# with open('test_quicksort_query_add_top50_1127.tsv', 'r') as f:
+with open('result.tsv', 'r') as f: # Put your file path here
     for line_number, line in enumerate(f, 1):
         try:
             qid, did, score = line.strip().split('\t')
@@ -28,23 +27,3 @@ for VALIDATION_METRIC in ['recip_rank', 'ndcg_cut_10', 'ndcg_cut_20', 'P_20']:
         evaluator = pytrec_eval.RelevanceEvaluator(qrels, {VALIDATION_METRIC})
         eval_scores = evaluator.evaluate(top_run)
         print(VALIDATION_METRIC, top_k, mean([d[VALIDATION_METRIC] for d in eval_scores.values()]))
-
-'''
-result_full_ori
-recip_rank 5 0.30888968481375356
-recip_rank 10 0.2580874494019193
-recip_rank 20 0.1837278468535288
-recip_rank 100 0.050449305837780324
-ndcg_cut_10 5 0.36826857093848087
-ndcg_cut_10 10 0.3552193766497411
-ndcg_cut_10 20 0.2904480618889735
-ndcg_cut_10 100 0.02193953383392664
-ndcg_cut_20 5 0.36826857093848087
-ndcg_cut_20 10 0.3552193766497411
-ndcg_cut_20 20 0.3161615544149717
-ndcg_cut_20 100 0.11386934734897264
-P_20 5 0.029183381088825216
-P_20 10 0.035193409742120346
-P_20 20 0.04042263610315187
-P_20 100 0.022915472779369627
-'''
